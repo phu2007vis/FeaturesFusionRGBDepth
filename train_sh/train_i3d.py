465142445ff81bbada68038f5b51e64a13d9963f
phuoc <21013187@st.phenikaa-uni.edu.vn>
2024-06-07 18:29:49 +0700

    update train_id3d.py

--- train_sh/train_i3d.py
+++ train_sh/train_i3d.py
@@ -4,20 +4,21 @@ import sys
 import argparse
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-from torch.autograd import Variable
 from tqdm import tqdm
 import resources.utils.rgb_dataset as dsl
 import random
-import numpy as np
 import datetime
 from resources.i3d.pytorch_i3d import InceptionI3d
 import eval as ev
 import yaml
-os.makedirs('logs',exist_ok=True)
 import logging.handlers
 from resources.utils import *
+
+
+
+# save infomation during training
+os.makedirs('logs',exist_ok=True)
 log_filename = datetime.datetime.now().strftime("sc_%d_%m_%H_%M_%S.log")
 log_filepath = os.path.join("logs", log_filename)
 
@@ -29,30 +30,46 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)
     ],force=True
 )
-
+# print some thinh when error 
 def handle_exception(exc_type, exc_value, exc_traceback):
     # Custom exception handling logic here
     logging.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
     # Call the default handler
     sys.__excepthook__(exc_type, exc_value, exc_traceback)
-
 sys.excepthook = handle_exception
 
-def run(init_lr=0.001, max_steps=200000, device = "cuda", root="/work/21010294/DepthData/OutputSplitAbsoluteVer2/", batch_size=8,
-        name='i3d-rgb', n_frames=200, elog=None,seed=42, cache=None,num_workers = 8):
+def run(init_lr,
+        max_steps,
+        device,
+        root,
+        batch_size,
+        n_frames,
+        num_workers,
+        seed = 42,
+        cache=None,
+        elog=None,
+        name='i3d-rgb'):
+    
     loss_fn = nn.CrossEntropyLoss()
-    random.seed(seed)
+    
     HEIGHT = 224
     WIDTH = 224
+    
     dataset = dsl.DSL(root, height=HEIGHT, width=WIDTH, n_frames=n_frames,random_seed=seed, cache_folder=cache)
     person_list = dataset.get_persons()
+    
+    random.seed(seed)
     random.shuffle(person_list)
+    
     val_index = int(len(person_list) * 0.7)
     test_index = int(len(person_list) * 0.8)
+    
     train_persons = person_list[:val_index]
     val_persons =   person_list[val_index:test_index]
     test_persons =  person_list[test_index:]
+    
     save_model = elog.get_path() + f"/{name}_"
+    
     print(f"Train: {len(train_persons)}",
         f" Val: {len(val_persons)}",
         f" Test: {len(test_persons)}")
@@ -79,125 +96,106 @@ def run(init_lr=0.001, max_steps=200000, device = "cuda", root="/work/21010294/D
 
     test_ds = dataset.get_generator(test_filter,mode = "valid")
     test_dl = torch.utils.data.DataLoader(test_ds, batch_size=batch_size,  num_workers=num_workers, pin_memory=True)
-    visualize_rgb(train_dl,"visualize",percent_visualize=0.5)
-    exit()
-    dataloaders = {'train': train_dl, 'val': val_dl, 'test': test_dl}
     
-
+    dataloaders = {'train': train_dl, 'val': val_dl, 'test': test_dl}
     num_classes = len(dataset.get_classes())
- 
+    
+    #turn on this for visualize
+    # visualize_rgb(train_dl,"visualize",percent_visualize=0.5)
+    # exit()
+    
     model = InceptionI3d(400, in_channels=3)
-    #load pretrained model
-    # model.load_state_dict(torch.load('models/rgb_imagenet.pt'))
     model.replace_logits(num_classes)
     model.to(device)
     model = nn.DataParallel(model)
     print(f"Train on {device}")
+    
     lr = init_lr
     optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=0.0000001)
-    checkpoints = []
-    
+
     num_steps_per_update = 4 # accum gradient
     steps = 0
-    # train it
-    pbar = tqdm(total=max_steps)
-    while steps < max_steps:#for epoch in range(num_epochs):
-        print( 'Step {}/{}'.format(steps, max_steps))
-        print( '-' * 10)
-
+    
+    for epoch in range(max_steps):#for epoch in range(num_epochs):
         # Each epoch has a training and validation phase
         for phase in ['train','val']:
-            if phase == 'train':
-                model.train()
-            else:
-                model.train()  # Set model to evaluate mode
-                
-            tot_loss = 0.0
-            num_iter = 0
-            optimizer.zero_grad()
-
+            
             if phase == 'train':
                 continue
-                for data in dataloaders[phase]:
-                    num_iter += 1
-                    # get the inputs
-                    inputs, labels = data
-
-                    # wrap them in Variable
-                    inputs = inputs.to(device)
-                    t = inputs.size(2)
-                    labels = labels.to(device)
-                    per_frame_logits = model(inputs)
-                    # upsample to input size
+                #get current learning rate
+                lr = optimizer.param_groups[0]['lr']
+                optimizer.zero_grad()
+                model.train()
+                
+                #reset parameter
+                tot_loss = 0.0
+                num_iter = 0
+                
+                #create process bar
+                pbar = tqdm(dataloaders[phase],total=len(dataloaders[phase]))
+                
+                for inputs, labels in pbar:
                     
+                    num_iter += 1
+
+                    # move to device ('cpu' or 'gpu' - 'cuda' )
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+                    
+                    #forward
+                    per_frame_logits = model(inputs)
+            
+                    #caculate loss
                     loss = loss_fn(per_frame_logits,labels)/num_steps_per_update
-                    tot_loss += loss.data.item()
+                    
+                    #caculate gradient 
                     loss.backward()
-                    if num_iter == num_steps_per_update and phase == 'train':
-                        steps += 1
-                        pbar.update(1)
-                        num_iter = 0
+                    
+                    #convert to float and add to total loss
+                    tot_loss += loss.data.item()
+                    
+                    # update each num_steps_per_update batch
+                    if num_iter == num_steps_per_update :
+                        
+                        #update weight
                         optimizer.step()
                         optimizer.zero_grad()
                         
-                        if steps % 10 == 0:
-                            info  =f"{steps} {tot_loss}" 
-                            pbar.set_description(info)
-                            elog.add_loss_entry(info)
-                            # save model
-                            torch.save(model.module.state_dict(), save_model+str(steps).zfill(6)+'.pt')
-                            #add checkpoint
-                            
-                            checkpoints.append(save_model+str(steps).zfill(6)+'.pt')
-                            if len(checkpoints) > 3:
-                                os.remove(checkpoints.pop(0))
-                        tot_loss =  0
-
+                        #print to screen
+                        info  =f"{epoch}/{max_steps} , lr : {lr} , train loss : {tot_loss}" 
+                        pbar.set_description(info)
+                        pbar.set_postfix()
+                        
+                        # reset parameters
+                        num_iter = 0
+                        tot_loss = 0
+                        
+     
+           # torch.save(model.module.state_dict(), save_model+'last.pt')
             if phase == 'val':
-              
-                print("Evaluating")
+         
+                
                 model.eval()
                 logits = []
                 with torch.no_grad():
-                    for data in dataloaders[phase]:
+                    for data in tqdm(dataloaders[phase],desc = "Evaluating"):
                         inputs, labels = data
                         inputs = inputs.to(device)
-                        labels = (inputs.to(device).cpu().max(1)[1]).numpy()
+                        labels = (labels.to(device).cpu().max(1)[1]).numpy()
                         
                         per_frame_logits = model(inputs)
-                        logit = nn.functional.softmax(per_frame_logits,dim = 1)
-                        logit = logit.max(1)[1].cpu().numpy()
+                      
+                        logit = per_frame_logits.max(-1)[1].cpu().numpy()
+                       
+                            
                         for i in range(len(logit)):
                             logits.append([logit[i], labels[i]])
-                    import pdb;pdb.set_trace()
+                    
                     elog.evaluate(phase,steps,logits, dataset.get_classes())
                 model.train()
     
-    pbar.close()
     #save model
     torch.save(model.module.state_dict(), save_model+f'i3d_train_final.pt')
-    #test
-    model.eval()
-    print("Testing")
-    logits = []
-    for data in dataloaders['test']:
-        inputs, labels = data
-        inputs = Variable(inputs.cuda())
-        t = inputs.size(2)
-        labels = Variable(labels.cuda())
-        per_frame_logits = model(inputs)
-        # upsample to input size
-        per_frame_logits = F.upsample(per_frame_logits, t, mode='linear')
-        logit = per_frame_logits[0].data.cpu().numpy()
-        classif = torch.max(per_frame_logits, dim=2)[0]
-        classif = classif[0].data.cpu().numpy()
-        label = torch.max(labels, dim=2)[0]
-        labels = label[0].data.cpu().numpy()
-        labels = np.argmax(labels)
-        predicted = np.argmax(classif)
-
-        logits.append([logit, classif,predicted, labels])
-    elog.evaluate('test',steps,logits,dataset.get_classes())
 
 
 
@@ -208,13 +206,26 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--root', type=str, help='root directory of the dataset', default=r"/work/21013187/SignLanguageRGBD/ViSLver2/Processed")
     parser.add_argument('-n', '--n_frames', type=int, help='n frame', default= 72)
     parser.add_argument('-c', '--cache', type=str, help='cache directory', default=None)
-    parser.add_argument('-s', '--seed', type=int, help='seed', default=42)
-    parser.add_argument('-a', '--a_config', type=str, help='spatial augumentation config', default="train_sh/config/spatial_augument_config.yaml")
-    
+    parser.add_argument('--seed', type=int, help='seed', default=42)
+    parser.add_argument('--a_config', type=str, help='spatial augumentation config', default="train_sh/config/spatial_augument_config.yaml")
+    parser.add_argument('--lr',type=float,default =0.001, help='init learning rate')
+    parser.add_argument('--epochs', type=int, help='number of training epochs', default=1000)
+    parser.add_argument('--batch_size', type=int, help='batch_size', default=9)
+    parser.add_argument('--num_workers', type=int, help='number of cpu load data', default=8)
+
     args = parser.parse_args()
     root = args.root
     n_frames = args.n_frames
     
     name = f"i3d-train{n_frames}"
     elog = ev.Eval(run_name=name)
-    run(device = args.device, root=root, name=name, n_frames=n_frames, elog=elog,seed=args.seed,cache=args.cache)
+    run(device = args.device, 
+        root=root, name=name,
+        n_frames=n_frames,
+        elog=elog,
+        seed=args.seed,
+        cache=args.cache,
+        init_lr = args.lr,
+        max_steps = args.epochs,
+        batch_size = args.batch_size,
+        num_workers = args.num_workers)
